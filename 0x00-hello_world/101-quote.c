f74afb1d93a59121a96698627b9cbc71a3ef9c2a
Oreoluwa Abiodun <oreoluwaabiodun@gmail.com>
2022-09-09 06:14:07 -0700

    My C-Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
  *
  *   * Return: 1 if success.
  *
- *    */
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
