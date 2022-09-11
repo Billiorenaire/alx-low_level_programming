baa6cf5fc095dae860b5b51d8a35886a0b06a888
Oreoluwa Abiodun <oreoluwaabiodun@gmail.com>
2022-09-11 11:45:51 -0700

    C-variables,if,else,while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,6 +9,7 @@
  * main - Prints all the numbers of base 16 in lowercase.
  *
  * Return: Always 0
+ *
  */
 int main(void)
 {
@@ -17,10 +18,10 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	
+
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-	
+
 	putchar('\n');
 
 	return (0);
