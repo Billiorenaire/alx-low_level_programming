062e0a73b3b0d97b7742717ec4f7572c24502ea8
Oreoluwa Abiodun <oreoluwaabiodun@gmail.com>
2022-09-19 21:15:14 -0700

    C-pointers,arrays,strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *, int *);
+int _strlen(char *);
+void _puts(char *);
+void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
