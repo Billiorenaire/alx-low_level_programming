5e0e45fa5c872ebd811e324801a5a560ac846aa1
Oreoluwa Abiodun <oreoluwaabiodun@gmail.com>
2022-10-25 04:48:38 -0700

    C-more singly linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -26,3 +33,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 
+#endif /* LISTS_H */
